c6d844db0aad13fc6353e022cd4397bdd709e472
personforme <73250608+personforme@users.noreply.github.com>
2020-11-02 22:04:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 #sudo /etc/init.d/gunicorn -b 0.0.0.0:8080 hello:app
-sudo gunicorn -b 0.0.0.0:8080 -c /home/box//web/hello.py hello:application
+#sudo gunicorn -b 0.0.0.0:8080 -c /home/box//web/hello.py hello:application
+sudo gunicorn -b 0.0.0.0:8080 -c hello.py hello &
