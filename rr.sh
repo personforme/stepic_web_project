2087dc6cff818d89daa059e78ba037babd8c3d40
personforme <73250608+personforme@users.noreply.github.com>
2020-11-03 19:54:00 +0300

    Update rr.sh

--- rr.sh
+++ rr.sh
@@ -1,5 +1,6 @@
-#cp /etc/nginx/nginx.conf /home/box//web/etc
-cp /etc/nginx/sites-enabled/default /home/box//web/etc
+cp /etc/nginx/nginx.conf /home/box//web/arc
+cp /etc/nginx/sites-enabled/default /home/box//web/arc
+cp /home/box//web/etc/default /etc/nginx/sites-enabled
 chmod a+x init.sh
 sudo find /home/box/web -type d -exec chmod 755 {} \; -print
 sudo find /home/box/web -type f -exec chmod 644 {} \; -print
