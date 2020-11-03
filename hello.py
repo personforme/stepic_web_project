619dfb829faa078dadc40e5266cc1a6222bc3004
personforme <73250608+personforme@users.noreply.github.com>
2020-11-03 19:34:54 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 def app(environ, start_response):
   status = '200 OK'
-  headers = [
-  ('Content-Type', 'text/plain')
-  ]
+  headers = [('Content-Type', 'text/plain')]
   start_response(status, headers )
   data = environ.get('QUERY_STRING')
   body = [bytes(i+'\n','ascii') for i in data.split('&')]
