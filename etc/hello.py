6518a91fed103758b07175ced28c89d442620429
personforme <73250608+personforme@users.noreply.github.com>
2020-11-03 19:58:38 +0300

    Create hello.py

---
+++ etc/hello.py
@@ -0,0 +1,7 @@
+def app(environ, start_response):
+  status = '200 OK'
+  headers = [('Content-Type', 'text/plain')]
+  start_response(status, headers )
+  data = environ.get('QUERY_STRING')
+  body = [bytes(i+'\n','ascii') for i in data.split('&')]
+  return body
