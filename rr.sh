eda7bf872746171d8d07e3d8ee29e5f6041921f3
personforme <73250608+personforme@users.noreply.github.com>
2020-11-03 20:10:46 +0300

    Update rr.sh

--- rr.sh
+++ rr.sh
@@ -1,5 +1,6 @@
 cp /etc/nginx/nginx.conf /home/box//web/arc
 cp /etc/nginx/sites-enabled/default /home/box//web/arc
+cp /etc/gunicorn.d/hello.py /home/box//web/arc
 cp /home/box//web/etc/default /etc/nginx/sites-enabled
 chmod a+x init.sh
 sudo find /home/box/web -type d -exec chmod 755 {} \; -print
