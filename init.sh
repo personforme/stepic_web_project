ff65e2d65c6a7c918f40ca93d2ab33a2c50c3e98
personforme <73250608+personforme@users.noreply.github.com>
2020-11-04 21:40:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-#sudo rm  /etc/nginx/sites-enabled/default
+sudo rm  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 
