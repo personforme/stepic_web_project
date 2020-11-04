2c1bb01448c954dc7e832db1737a3902855aa68c
personforme <73250608+personforme@users.noreply.github.com>
2020-11-04 21:21:23 +0300

    Update init2.sh

--- init2.sh
+++ init2.sh
@@ -1,3 +1,4 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo cp /home/box/web/new/nginx.conf /etc/nginx/sites-enabled/ && sudo mv nginx.conf default
+#sudo cp /home/box/web/new/nginx.conf /etc/nginx/sites-enabled/ && sudo mv nginx.conf default
+sudo cp /home/box/web/new/nginx.conf /etc/nginx/
 sudo /etc/init.d/nginx restart
