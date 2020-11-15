3b9866cf7d2a9bbada3193e4ba6b7f421b17e85d
personforme <73250608+personforme@users.noreply.github.com>
2020-11-15 21:46:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,12 +8,16 @@ sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 #sudo gunicorn -b 0.0.0.0:8080 -c hello.py hello &
 #gunicorn -bind='0.0.0.0:8080' -c hello.py hello:app
 #gunicorn -b 0.0.0.0:8080 hello:application
-gunicorn -b 0.0.0.0:8080 hello &
-cd /home/box/web/ask
+
 #gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
+
 
 #sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 #gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
 
+##gunicorn -b 0.0.0.0:8080 hello &
+##cd /home/box/web/ask
+
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application
